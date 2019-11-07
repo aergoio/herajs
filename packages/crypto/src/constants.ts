8bc3e2dba926e3a75d76bac1dfe7c844e479ebd1
Paul Grau <paul@blocko.io>
2019-11-07 11:25:11 +0900

    Crypto: change BIP44 ID to 441
    See https://github.com/satoshilabs/slips/blob/master/slip-0044.md

--- packages/crypto/src/constants.ts
+++ packages/crypto/src/constants.ts
@@ -5,6 +5,6 @@ export const ADDRESS_PREFIXES = {
 
 export const ACCOUNT_NAME_LENGTH = 12;
 
-export const BIP44_ID = 442;
+export const BIP44_ID = 441;
 
 export const WALLET_HDPATH = `m/44'/${BIP44_ID}'/0'/0/`;
