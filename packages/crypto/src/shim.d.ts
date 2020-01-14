65912f1fbe08ff803e2c542439e34a27b1452362
Paul Grau <paul@blocko.io>
2020-01-14 14:35:21 +0900

    Crypto: Remove unneeded shim

--- packages/crypto/src/shim.d.ts
+++ packages/crypto/src/shim.d.ts
@@ -2,7 +2,3 @@ declare module 'bs58check' {
     export function decode(input: string): Buffer;
     export function encode(input: Buffer): string;
 }
-
-declare module '@web3-js/scrypt-shim' {
-    export default function scrypt(key: string | Buffer, salt: string | Buffer, N: number, r: number, p: number, dkLen: number): Buffer;
-}
