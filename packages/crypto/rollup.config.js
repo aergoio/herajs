942e9a3ac542b05b181b7945a4030e34a7b4791b
Paul Grau <paul@blocko.io>
2020-01-14 14:26:11 +0900

    Update packages

--- packages/crypto/rollup.config.js
+++ packages/crypto/rollup.config.js
@@ -1,9 +1,9 @@
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import builtins from 'rollup-plugin-node-builtins';
 import globals from 'rollup-plugin-node-globals';
-import json from 'rollup-plugin-json';
+import json from '@rollup/plugin-json';
 import ignore from 'rollup-plugin-ignore';
 import { terser } from 'rollup-plugin-terser';
 //import visualizer from 'rollup-plugin-visualizer';
